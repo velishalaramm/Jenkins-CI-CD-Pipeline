d8e5a623f60416a40c6b255be60526e69608c1d0
velishalaramm <velishalaramm@gmail.com>
2026-09-18 18:18:22 +0530

    sonar install

---
+++ Sonar-install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+#Launch an instance with 9000 and t2.medium
+cd /opt/
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-8.9.6.50800.zip
+unzip sonarqube-8.9.6.50800.zip
+yum install java-17-amazon-corretto -y
+useradd sonar
+chown sonar:sonar sonarqube-8.9.6.50800 -R
+su - sonar
